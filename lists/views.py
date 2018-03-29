d8ff56f5464dd67f10c2131524c8d921a5974551
Xiaorui <xiaorui@mit.edu>
2018-03-29 17:22:36 -0400

    new URL: /--/add_item + view for exisiting lists

--- lists/views.py
+++ lists/views.py
@@ -34,15 +34,28 @@ def home_page(request):
     return render(request, 'home.html',)
 
 
-def view_list(request):
+def view_list(request, list_id):
 
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
+    # version 1
+    # items = Item.objects.all()
 
+    # # version 2
+    # list_ = List.objects.get(id=list_id)
+    # items = Item.objects.filter(list=list_)
+    # return render(request, 'list.html', {'items': items})
+
+    list_ = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
 
     # This view is just like a redirect station i think
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect('/lists/%d/' % list_.id, )
+
+def add_item(request, list_id):
+
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/%d/' % list_.id, )
